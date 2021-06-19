aa3838805914db3a5fef16d49a8194f06cb828a3
student <abhijeet.kimt@gmail.com>
2021-06-19 17:00:46 +0000

    mvn pipeline commit

---
+++ src/main/java/com/simplilearn/springpipeline/SpringpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.springpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpipelineApplication.class, args);
+	}
+
+}
